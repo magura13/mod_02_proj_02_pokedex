fb936479a93b85630caa5ecc5f629731ecb05e9a
magura13 <tiagolelis_@hotmail.com>
2022-04-21 11:46:52 -0300

    Aula-02

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
 const pokedex = [
   {
@@ -36,6 +37,14 @@ const pokedex = [
 
 app.get("/", (req, res) => {
   res.render("index", { pokedex });
+});
+
+app.post("/add", (req, res) => {
+  const pokemon = req.body;
+  pokedex.push(pokemon);
+
+
+  res.redirect("/");
 });
 
 //Rotas
